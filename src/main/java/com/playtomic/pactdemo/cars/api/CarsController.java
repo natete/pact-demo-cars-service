8db66c8c3b86b34f14257be02341f58d06614c29
Nacho González Bullón <natete981@gmail.com>
2022-05-17 09:05:11 +0200

    Fix tests

--- src/main/java/com/playtomic/pactdemo/cars/api/CarsController.java
+++ src/main/java/com/playtomic/pactdemo/cars/api/CarsController.java
@@ -2,6 +2,7 @@ package com.playtomic.pactdemo.cars.api;
 
 import com.playtomic.pactdemo.cars.domain.Car;
 import com.playtomic.pactdemo.cars.service.CarsService;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
